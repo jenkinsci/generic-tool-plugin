aaf39c5f77e36b993257616bacd97afa0e52ec9e
Daniel Beck <daniel-beck@users.noreply.github.com>
2022-07-05 17:10:00 +0200

    Initial source code commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Generic Tool Plugin
+
+## Introduction
+
+This plugin provides a "Generic Tool" tool installation for use in Pipelines.
+
+It's a lighter weight alternative to installing plugins like Ant, Groovy, etc. when all you need is the tool installation definition for use with the `tool` step.
+
+## Getting started
+
+The tool configuration can be found in _Manage Jenkins » Global Tool Configuration » Generic Tool_.
+
+The tool installations configured there can be used with the `tool` step in Pipelines. 
+
+## Contributing
+
+Refer to our [contribution guidelines](https://github.com/jenkinsci/.github/blob/master/CONTRIBUTING.md)
+
+## LICENSE
+
+Licensed under MIT, see [LICENSE](LICENSE.md)
---
+++ src/main/java/io/jenkins/plugins/generic_tool/GenericToolInstallation.java
@@ -0,0 +1,42 @@
+package io.jenkins.plugins.generic_tool;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.model.EnvironmentSpecific;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.slaves.NodeSpecific;
+import hudson.tools.ToolDescriptor;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolProperty;
+import java.io.IOException;
+import java.util.List;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class GenericToolInstallation extends ToolInstallation implements NodeSpecific<GenericToolInstallation>, EnvironmentSpecific<GenericToolInstallation> {
+
+    @DataBoundConstructor
+    public GenericToolInstallation(String name, String home, List<? extends ToolProperty<?>> properties) {
+        super(name, home, properties);
+    }
+
+    @Override
+    public GenericToolInstallation forEnvironment(EnvVars environment) {
+        return new GenericToolInstallation(getName(), environment.expand(getHome()), getProperties().toList());
+    }
+
+    @Override
+    public GenericToolInstallation forNode(@NonNull Node node, TaskListener log) throws IOException, InterruptedException {
+        return new GenericToolInstallation(getName(), translateFor(node, log), getProperties().toList());
+    }
+
+    @Extension
+    public static class DescriptorImpl extends ToolDescriptor<GenericToolInstallation> {
+        @NonNull
+        @Override
+        public String getDisplayName() {
+            return "Generic Tool";
+        }
+    }
+}
